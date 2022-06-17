a7c22fcc3930631ece000d77276b33348329455a
Matthias Krompaß <matthias.krompass@adbv-a.bayern.de>
2022-06-17 12:16:24 +0200

    10. Shared Data

--- www/resources/js/Shared/Layout.vue
+++ www/resources/js/Shared/Layout.vue
@@ -1,7 +1,13 @@
 <template>
   <section class="bg-gray-200 p-6">
     <header class="flex justify-between">
-      <h1 class="font-bold text-lg">My App</h1>
+      <div class="flex items-center">
+        <h1 class="font-bold text-lg">My App</h1>
+        <p class="text-sm ml-4">
+          <!-- Welcome back, {{ $page.props.auth.user.username }} -->
+          Welcome back, {{ username }}
+        </p>
+      </div>
       <Nav />
     </header>
   </section>
@@ -16,5 +22,11 @@ import Nav from "./Nav";
 
 export default {
   components: { Nav },
+
+  computed: {
+    username() {
+      return this.$page.props.auth.user.username
+    }
+  }
 };
 </script>
